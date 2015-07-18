c649cbd1cf7969056003d218b230736c61471a17
robin <robin@e7systems.com>
2015-07-18 07:17:01 -0600

    increased visibility of plot over "High" level category by increasing width of white outlining

--- RiverFlows/App/src/main/java/com/riverflows/view/HydroGraph.java
+++ RiverFlows/App/src/main/java/com/riverflows/view/HydroGraph.java
@@ -194,7 +194,7 @@ public class HydroGraph extends View {
         legendPadding = (int)(scaledDensity * 10f);
 
         plotPaint.setStrokeWidth(scaledDensity * 1.0f);
-        plotBgPaint.setStrokeWidth(scaledDensity * 2.0f);
+        plotBgPaint.setStrokeWidth(scaledDensity * 3.0f);
         forecastPaint.setStrokeWidth(scaledDensity * 2.0f);
 	}
 	
