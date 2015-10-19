db41f91893a7862e53b1c044ba98912f0bf8f439
robin <robin@e7systems.com>
2015-10-19 13:10:39 -0600

    fixed problem that was causing infinite loop for CODWR gages when ViewSite variable was null

--- RiverFlows/App/src/main/java/com/riverflows/SiteFragment.java
+++ RiverFlows/App/src/main/java/com/riverflows/SiteFragment.java
@@ -243,12 +243,13 @@ public class SiteFragment extends RoboFragment implements LoaderManager.LoaderCa
         }
 
         if (displayedSeries == null) {
-            displayedSeries = DataSourceController.getPreferredSeries(this.data);
             Log.d(Home.TAG, "No series found for " + getVariable());
-            if(!this.data.isComplete()) {
+            if(!this.data.isComplete() && getVariable() != null) {
                 //DataSetLoader will reload data specifically requesting the current variable
                 reload();
                 return;
+            } else {
+                displayedSeries = DataSourceController.getPreferredSeries(this.data);
             }
         }
 
