011c7af5647ee9c78219b2f1c0cb7d5cddbe83b9
Robin Johnson <robin.m.j@gmail.com>
2015-12-13 07:45:05 -0700

    tested scenario where user has legacy favorites but no destinations

--- RiverFlows/App/src/test/java/com/riverflows/HomeTest.java
+++ RiverFlows/App/src/test/java/com/riverflows/HomeTest.java
@@ -12,8 +12,10 @@ import com.riverflows.data.Favorite;
 import com.riverflows.data.FavoriteData;
 import com.riverflows.data.SiteData;
 import com.riverflows.data.UserAccount;
+import com.riverflows.db.FavoritesDaoImpl;
 import com.riverflows.factory.DestinationFacetFactory;
 import com.riverflows.factory.SiteDataFactory;
+import com.riverflows.factory.SiteFactory;
 import com.riverflows.wsclient.CODWRDataSource;
 import com.riverflows.wsclient.WsSession;
 import com.riverflows.wsclient.WsSessionManager;
@@ -138,6 +140,63 @@ public class HomeTest {
 
         List<FavoriteData> mockData = new ArrayList<FavoriteData>();
         final Favorite clearCreekFav = new Favorite(clearCreekKayak);
+        SiteData clearCreekData = SiteDataFactory.getClearCreekData();
+        FavoriteData clearCreekFavData = new FavoriteData(clearCreekFav,clearCreekData, CODWRDataSource.VTYPE_STREAMFLOW_CFS);
+        mockData.add(clearCreekFavData);
+
+        when(wsClient.dsControllerMock.getFavoriteData(anyList(), anyBoolean())).thenReturn(mockData);
+
+        Intent i = new Intent(RuntimeEnvironment.application, Home.class);
+
+        Home h = createHome(i);
+
+        SharedPreferences prefs = h.getSharedPreferences(WsSessionManager.PREFS_FILE_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString(WsSessionManager.PREF_ACCOUNT_NAME, "robin.m.j");
+        editor.commit();
+
+        Favorites favorites = null;
+
+        for(Fragment f : h.getSupportFragmentManager().getFragments()) {
+            if(f instanceof Favorites) {
+                favorites = (Favorites)f;
+            }
+        }
+
+        verify(wsClient.dsControllerMock).getFavoriteData(argThat(new ArgumentMatcher<List<Favorite>>() {
+            public boolean matches(Object list) {
+                //favorite should have a non-null local primary key before it is passed into getFavoriteData()
+
+                return ((List<Favorite>) list).get(0).getId() != null;
+            }
+
+            public String toString() {
+                return "[one favorite with a non-null ID]";
+            }
+        }), anyBoolean());
+
+        assertThat(favorites.getListView().getVisibility(), equalTo(View.VISIBLE));
+        assertThat(favorites.getListView().getEmptyView().getVisibility(), equalTo(View.GONE));
+
+    }
+
+    @Test
+    public void testEmptyDestinations() throws Exception {
+
+        UserAccount account = new UserAccount();
+        account.setEmail("robin.m.j@gmail.com");
+        account.setFacetTypes(4);
+        mockSessionManager.session = new WsSession("robin.m.j", account, "", System.currentTimeMillis() + 10 * 60 * 1000);
+
+        RoboGuice.overrideApplicationInjector(RuntimeEnvironment.application, wsClient, mockSessionManager);
+
+        when(wsClient.destinationFacetsMock.getFavorites(any(WsSession.class))).thenReturn(new ArrayList<DestinationFacet>());
+
+        List<FavoriteData> mockData = new ArrayList<FavoriteData>();
+        final Favorite clearCreekFav = new Favorite(SiteFactory.getClearCreek(), CODWRDataSource.VTYPE_STREAMFLOW_CFS.getId());
+
+        FavoritesDaoImpl.createFavorite(RuntimeEnvironment.application, clearCreekFav);
+
         SiteData clearCreekData = SiteDataFactory.getClearCreekData();
         FavoriteData clearCreekFavData = new FavoriteData(clearCreekFav,clearCreekData, CODWRDataSource.VTYPE_STREAMFLOW_CFS);
         mockData.add(clearCreekFavData);
