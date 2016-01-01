25806e3e3869811bccb9f30ee520dbe2f378efd4
Robin Johnson <robin.m.j@gmail.com>
2016-01-01 07:41:36 -0700

    increased granularity of error handling in CDECDataSource, tested getSiteData(List<Favorites>...

--- RiverFlows/Datasources-Tests/src/com/riverflows/wsclient/CDECDataSourceTest.java
+++ RiverFlows/Datasources-Tests/src/com/riverflows/wsclient/CDECDataSourceTest.java
@@ -2,9 +2,13 @@ package com.riverflows.wsclient;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 import junit.framework.TestCase;
 
+import com.riverflows.data.Favorite;
+import com.riverflows.data.FavoriteData;
 import com.riverflows.data.Reading;
 import com.riverflows.data.Series;
 import com.riverflows.data.Site;
@@ -19,6 +23,90 @@ public class CDECDataSourceTest extends TestCase {
 
 	static {
 		ds.setHttpClientWrapper(new MockCDECHttpClient());
+	}
+
+	public void testGetFavorites() throws Throwable {
+		Site cbr = new Site(new SiteId("CDEC","CBR"), null, null, null);
+
+		List<Favorite> favs = new ArrayList<Favorite>();
+		favs.add(new Favorite(cbr, CDECDataSource.VTYPE_FLOW.getId()));
+
+		List<FavoriteData> result = ds.getSiteData(favs, true);
+
+		assertEquals(favs.get(0), result.get(0).getFavorite());
+
+		SiteData data = result.get(0).getSiteData();
+
+		System.out.println("dataInfo:\n" + data.getDataInfo());
+
+		Series streamflow = data.getDatasets().get(CommonVariable.STREAMFLOW_CFS);
+
+		Reading lastObs = streamflow.getLastObservation();
+
+		DateFormat pdtFormat = SimpleDateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.LONG);
+		pdtFormat.setTimeZone(USTimeZone.PDT.getTimeZone());
+
+		assertEquals("4/17/12 12:00:00 PM GMT-07:00", pdtFormat.format(lastObs.getDate()));
+		assertEquals(2551.0d, lastObs.getValue());
+
+		assertEquals(167, streamflow.getReadings().size());
+
+		Series gauge_height = data.getDatasets().get(CommonVariable.GAUGE_HEIGHT_FT);
+
+		Reading firstObs = gauge_height.getReadings().get(0);
+		assertEquals("4/10/12 2:00:00 PM GMT-07:00", pdtFormat.format(firstObs.getDate()));
+		assertEquals(4.14d, firstObs.getValue());
+
+		Site ads = new Site(new SiteId("CDEC","ADS"), null, null, null);
+		data = ds.getSiteData(ads, new Variable[] { CDECDataSource.VTYPE_WIND_DR }, true);
+
+		System.out.println("dataInfo:\n" + data.getDataInfo());
+	}
+
+	public void testGetNonexistentFavorites() throws Throwable {
+		Site fake = new Site(new SiteId("CDEC","ABCDEFG"), null, null, null);
+		Site cbr = new Site(new SiteId("CDEC","CBR"), null, null, null);
+
+		List<Favorite> favs = new ArrayList<Favorite>();
+		favs.add(new Favorite(fake, CDECDataSource.VTYPE_BAT_VOL.getId()));
+		favs.add(new Favorite(cbr, CDECDataSource.VTYPE_FLOW.getId()));
+
+		List<FavoriteData> result = ds.getSiteData(favs, true);
+
+		assertEquals(favs.get(0), result.get(0).getFavorite());
+		assertEquals(favs.get(1), result.get(1).getFavorite());
+
+		SiteData data = result.get(0).getSiteData();
+
+		assertNull(data.getDatasets().get(CDECDataSource.VTYPE_BAT_VOL.getCommonVariable()).getLastObservation().getValue());
+		assertEquals("Datasource Down", data.getDatasets().get(CDECDataSource.VTYPE_BAT_VOL.getCommonVariable()).getLastObservation().getQualifiers());
+
+		data = result.get(1).getSiteData();
+
+		System.out.println("dataInfo:\n" + data.getDataInfo());
+
+		Series streamflow = data.getDatasets().get(CommonVariable.STREAMFLOW_CFS);
+
+		Reading lastObs = streamflow.getLastObservation();
+
+		DateFormat pdtFormat = SimpleDateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.LONG);
+		pdtFormat.setTimeZone(USTimeZone.PDT.getTimeZone());
+
+		assertEquals("4/17/12 12:00:00 PM GMT-07:00", pdtFormat.format(lastObs.getDate()));
+		assertEquals(2551.0d, lastObs.getValue());
+
+		assertEquals(167, streamflow.getReadings().size());
+
+		Series gauge_height = data.getDatasets().get(CommonVariable.GAUGE_HEIGHT_FT);
+
+		Reading firstObs = gauge_height.getReadings().get(0);
+		assertEquals("4/10/12 2:00:00 PM GMT-07:00", pdtFormat.format(firstObs.getDate()));
+		assertEquals(4.14d, firstObs.getValue());
+
+		Site ads = new Site(new SiteId("CDEC","ADS"), null, null, null);
+		data = ds.getSiteData(ads, new Variable[] { CDECDataSource.VTYPE_WIND_DR }, true);
+
+		System.out.println("dataInfo:\n" + data.getDataInfo());
 	}
 	
 	public void testGetSiteData() throws Throwable {
